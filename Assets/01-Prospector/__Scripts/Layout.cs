4df53998759c85920bdb2b555652b7238aac6333
JackTesoro <jt1107@scarletmail.rutgers.edu>
2024-03-28 20:13:06 -0400

    new build

--- Assets/01-Prospector/__Scripts/Layout.cs
+++ Assets/01-Prospector/__Scripts/Layout.cs
@@ -23,7 +23,7 @@ public class Layout : MonoBehaviour
     public List<SlotDef> slotDefs;
     public SlotDef drawPile;
     public SlotDef discardPile;
-    public string[] sortingLayerNames = new string[] {"Row0", "Row1", "Row2", "Row3", "Discard", "Draw"};
+    public string[] sortingLayerNames = new string[] {"Row0", "Row1", "Row2", "Row3", "Row4", "Row5", "Row6", "Discard", "Draw"};
 
     public void ReadLayout (string xmlText)
     {
